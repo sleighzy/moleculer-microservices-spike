e12b4ab512b993623117d3790d6b33d99e1af47b
Simon Leigh <Simon.Leigh@orionhealth.com>
2018-07-08 21:28:05 +1200

    Refectored the Orders service to use CQRS and event sourcing workflows. The service now contains a Kafka producer and consumer for handling events. The creation or updates of orders will publish the order and associated event to a Kafka topic. This replaces the original functionality whichh wrote directly to the MongoDB. The Kafka consumer within the service consumes these events and performs the database updates based on those. All requests to list orders or retrieve a single order are queried against the MongoDB directly. The means that other services can also subscribe to the orders topic to consume and process these same events.

--- services/orders.service.js
+++ services/orders.service.js
@@ -1,8 +1,10 @@
 const { Service } = require('moleculer');
+const { MoleculerError } = require('moleculer').Errors;
 const DbService = require('moleculer-db');
 const MongooseAdapter = require('moleculer-db-adapter-mongoose');
 const mongoose = require('mongoose');
-const { HighLevelProducer, KeyedMessage, KafkaClient } = require('kafka-node');
+const { HighLevelProducer, KeyedMessage, KafkaClient } = require('kafka-node'); // eslint-disable-line object-curly-newline
+const Kafka = require('kafka-node');
 
 class OrdersService extends Service {
   constructor(broker) {
@@ -57,11 +59,6 @@ class OrdersService extends Service {
         // No events
       },
 
-      // These entityX functions are called by the moleculer-db
-      // mixin when entities are created, updated, or deleted.
-      entityCreated: this.orderCreated,
-      entityUpdated: this.orderUpdated,
-
       created: this.serviceCreated,
       started: this.serviceStarted,
       stopped: this.serviceStopped,
@@ -79,8 +76,7 @@ class OrdersService extends Service {
       price,
       state: 'Pending',
     };
-    // This calls "orders.insert" which is the insert() function from the DbService mixin.
-    return ctx.call('orders.insert', { entity: order });
+    return this.sendEvent(order, 'OrderCreated');
   }
 
   rejectOrder(ctx) {
@@ -103,61 +99,13 @@ class OrdersService extends Service {
 
   // Private methods
   updateOrderState(ctx, id, state) {
-    this.logger.debug('Update order state:', id, state);
-    // This calls "orders.update" which is the update() function from the DbService mixin.
-    return ctx.call('orders.update', { id, state, updated: Date.now() });
+    return this.sendEvent({ id, state, updated: Date.now() }, 'OrderUpdated');
   }
 
-  orderCreated(order, ctx) { // eslint-disable-line no-unused-vars
-    this.logger.debug('Order created:', order);
-    return this.sendEvent(order, 'OrderCreated');
-  }
-
-  orderUpdated(order, ctx) { // eslint-disable-line no-unused-vars
-    this.logger.debug('Order updated:', order);
-    return this.sendEvent(order, 'OrderUpdated');
-  }
-
-  sendEvent(order, type) {
-    return new this.Promise((resolve, reject) => {
-      if (!order) {
-        reject('No order found when sending event.');
-      }
-      if (!type) {
-        reject('No event type specified when sending event.');
-      }
-
-      const data = order;
-      data.eventType = type;
-      this.logger.debug('data:', data);
-      const payload = this.createPayload(data);
-      this.producer.send(payload, (error, result) => {
-        this.logger.debug('Sent payload to Kafka:', JSON.stringify(payload));
-        if (error) {
-          reject(error);
-        } else {
-          this.logger.debug('Result:', result);
-          resolve(result);
-        }
-      });
-    });
-  }
-
-  createPayload(data) {
-    const message = new KeyedMessage(data.id, JSON.stringify(data));
-    return [{
-      topic: this.settings.ordersTopic,
-      messages: [message],
-      attributes: 1, // Use GZip compression for the payload.
-      timestamp: Date.now(),
-    }];
-  }
-
-  serviceCreated() {
-    this.logger.debug('Orders service created.');
-  }
-
-  serviceStarted() {
+  /**
+   * Function to create a Kafka producer to publish order events to a kafka topic.
+   */
+  startKafkaProducer() {
     const client = new KafkaClient({
       kafkaHost: this.settings.bootstrapServer,
     });
@@ -175,6 +123,117 @@ class OrdersService extends Service {
     });
 
     this.producer.on('error', error => this.logger.error(error));
+  }
+
+  /**
+   * Function to create a Kafka consumer and start consuming events off the order topic.
+   */
+  startKafkaConsumer() {
+    const kafkaOptions = {
+      kafkaHost: this.settings.bootstrapServer, // connect directly to kafka broker (instantiates a KafkaClient)
+      batch: undefined, // put client batch settings if you need them (see Client)
+      // ssl: true, // optional (defaults to false) or tls options hash
+      groupId: 'kafka-node-orders',
+      sessionTimeout: 15000,
+      // An array of partition assignment protocols ordered by preference.
+      // 'roundrobin' or 'range' string for built ins (see below to pass in custom assignment protocol)
+      protocol: ['roundrobin'],
+
+      // Set encoding to 'buffer' for binary data.
+      // encoding: 'buffer',
+
+      // Offsets to use for new groups other options could be 'earliest' or 'none' (none will emit an error if no offsets were saved)
+      // equivalent to Java client's auto.offset.reset
+      fromOffset: 'latest', // default
+
+      // how to recover from OutOfRangeOffset error (where save offset is past server retention) accepts same value as fromOffset
+      outOfRangeOffset: 'earliest', // default
+      migrateHLC: false, // for details please see Migration section below
+      migrateRolling: true,
+    };
+
+    this.consumer = new Kafka.ConsumerGroup(kafkaOptions, this.settings.ordersTopic);
+    this.consumer.on('message', message => this.processEvent(message.value));
+    this.consumer.on('error', err => this.Promise.reject(new MoleculerError(`${err.message} ${err.detail}`, 500, 'CONSUMER_MESSAGE_ERROR')));
+
+    process.on('SIGINT', () => this.consumer.close(true));
+  }
+
+  /**
+   * Function to consume order events from a Kafka topic and process them.
+   *
+   * @param {Object} event event containing event tpye and order information.
+   * @returns {Promise}
+   */
+  processEvent(event) {
+    this.logger.debug(event);
+    const orderEvent = JSON.parse(event);
+
+    return new this.Promise((resolve) => {
+      if (orderEvent.eventType === 'OrderCreated') {
+        // This calls "orders.insert" which is the insert() function from the DbService mixin.
+        resolve(this.broker.call('orders.insert', { entity: orderEvent.order }));
+      } else if (orderEvent.eventType === 'OrderUpdated') {
+        // This calls "orders.update" which is the update() function from the DbService mixin.
+        resolve(this.broker.call('orders.update', orderEvent.order));
+      } else {
+        this.logger.error('eventType:', orderEvent.eventType);
+      }
+    });
+  }
+
+  /**
+   * Function to publish order event data to a Kafka topic.
+   *
+   * @param {Object} order the order information
+   * @param {String} type the event type
+   * @returns {Promise}
+   */
+  sendEvent(order, type) {
+    return new this.Promise((resolve, reject) => {
+      if (!order) {
+        reject('No order found when sending event.');
+      }
+      if (!type) {
+        reject('No event type specified when sending event.');
+      }
+
+      const payload = this.createPayload({ order, eventType: type });
+      this.producer.send(payload, (error, result) => {
+        this.logger.debug('Sent payload to Kafka:', JSON.stringify(payload));
+        if (error) {
+          reject(error);
+        } else {
+          this.logger.debug('Result:', result);
+          resolve(result);
+        }
+      });
+    });
+  }
+
+  /**
+   * Function to create a Kafka message payload with an order event.
+   *
+   * @param {any} data Order event data.
+   * @returns {Array} an array of order event messages.
+   */
+  createPayload(data) {
+    const message = new KeyedMessage(data.id, JSON.stringify(data));
+    return [{
+      topic: this.settings.ordersTopic,
+      messages: [message],
+      attributes: 1, // Use GZip compression for the payload.
+      timestamp: Date.now(),
+    }];
+  }
+
+  serviceCreated() {
+    this.logger.debug('Orders service created.');
+  }
+
+  serviceStarted() {
+    this.startKafkaProducer();
+    this.startKafkaConsumer();
 
     this.logger.debug('Orders service started.');
   }
